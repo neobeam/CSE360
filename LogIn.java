7e35c31653420070121f6b903ea3ebbd235e739e
neobeam <74326174+neobeam@users.noreply.github.com>
2023-04-02 15:39:59 -0700

    Add files via upload

---
+++ LogIn.java
@@ -0,0 +1,43 @@
+package application;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
+
+public class LogIn {
+	public LogIn() {
+		
+	}
+
+
+@FXML
+public Button button;
+@FXML
+private Label wrongLogIn;
+@FXML
+private TextField username;
+@FXML
+private PasswordField password;
+
+public void userLogIn(ActionEvent event) throws IOException {
+	checkLogin();
+}
+private void checkLogin() throws IOException {
+	Main m = new Main();
+	if(username.getText().toString().equals("yjkim") && password.getText().toString().equals("1234")) {
+		wrongLogIn.setText("Success!");
+		m.changeScene("afterLogin.fxml");
+	} else if(username.getText().isEmpty() && password.getText().isEmpty()) {
+		wrongLogIn.setText("Please enter your data.");
+	} else {
+		wrongLogIn.setText("Wrong username or password!");
+	}
+}
+
+}
---
+++ Main.java
@@ -0,0 +1,44 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+	private static Stage stg;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			stg = primaryStage;
+			Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+			primaryStage.setTitle("YJ W25");
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void changeScene(String fxml) throws IOException {
+		Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+		stg.getScene().setRoot(pane);
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}         
+	
+	
+	
+}
+
